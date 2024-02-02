OpenFOAM/OpenFOAM-11

multiphaseEuler::phaseModel: Changed the phase-fraction field construction to work around a problem with Clang
The Clang compiler does not use std::move to transfer the result of the ternary
operator into the phase-fraction field resulting in it not being registered to
the database.  To work around this limitation/bug the ternary operator is now
provided with tmp fields the result of which is passed with an IOobject to the
final field constructor to ensure it is registered and the IO options set
correctly.

applications/modules/multiphaseEuler/phaseSystems/phaseModel/phaseModel/phaseModel.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2015-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2015-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -46,33 +46,69 @@ Foam::phaseModel::phaseModel
     const label index
 )
 :
+    // volScalarField
+    // (
+    //     referencePhase
+    //   ? volScalarField
+    //     (
+    //         IOobject
+    //         (
+    //             IOobject::groupName("alpha", phaseName),
+    //             fluid.mesh().time().name(),
+    //             fluid.mesh(),
+    //             IOobject::NO_READ,
+    //             IOobject::AUTO_WRITE
+    //         ),
+    //         fluid.mesh(),
+    //         dimensionedScalar(dimless, 0)
+    //     )
+    //   : volScalarField
+    //     (
+    //         IOobject
+    //         (
+    //             IOobject::groupName("alpha", phaseName),
+    //             fluid.mesh().time().name(),
+    //             fluid.mesh(),
+    //             IOobject::MUST_READ,
+    //             IOobject::AUTO_WRITE
+    //         ),
+    //         fluid.mesh()
+    //     )
+    // ),
+    // Replaced by the following because the Clang compiler does not use
+    // std::move to transfer the result of the ternary operator
     volScalarField
     (
-        referencePhase
-      ? volScalarField
+        IOobject
         (
-            IOobject
-            (
-                IOobject::groupName("alpha", phaseName),
-                fluid.mesh().time().name(),
-                fluid.mesh(),
-                IOobject::NO_READ,
-                IOobject::AUTO_WRITE
-            ),
+            IOobject::groupName("alpha", phaseName),
+            fluid.mesh().time().name(),
+            fluid.mesh(),
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        referencePhase
+      ? volScalarField::New
+        (
+            IOobject::groupName("alpha", phaseName),
             fluid.mesh(),
             dimensionedScalar(dimless, 0)
         )
-      : volScalarField
+      : tmp<volScalarField>
         (
-            IOobject
+            new volScalarField
             (
-                IOobject::groupName("alpha", phaseName),
-                fluid.mesh().time().name(),
-                fluid.mesh(),
-                IOobject::MUST_READ,
-                IOobject::AUTO_WRITE
-            ),
-            fluid.mesh()
+                IOobject
+                (
+                    IOobject::groupName("alpha", phaseName),
+                    fluid.mesh().time().name(),
+                    fluid.mesh(),
+                    IOobject::MUST_READ,
+                    IOobject::AUTO_WRITE,
+                    false
+                ),
+                fluid.mesh()
+            )
         )
     ),
 

OpenFOAM/OpenFOAM-8

applications/solvers/multiphase/multiphaseEulerFoam/phaseSystems/phaseModel/phaseModel/phaseModel.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2015-2020 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2015-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -46,33 +46,69 @@ Foam::phaseModel::phaseModel
     const label index
 )
 :
+    // volScalarField
+    // (
+    //     referencePhase
+    //   ? volScalarField
+    //     (
+    //         IOobject
+    //         (
+    //             IOobject::groupName("alpha", phaseName),
+    //             fluid.mesh().time().timeName(),
+    //             fluid.mesh(),
+    //             IOobject::NO_READ,
+    //             IOobject::AUTO_WRITE
+    //         ),
+    //         fluid.mesh(),
+    //         dimensionedScalar(dimless, 0)
+    //     )
+    //   : volScalarField
+    //     (
+    //         IOobject
+    //         (
+    //             IOobject::groupName("alpha", phaseName),
+    //             fluid.mesh().time().timeName(),
+    //             fluid.mesh(),
+    //             IOobject::MUST_READ,
+    //             IOobject::AUTO_WRITE
+    //         ),
+    //         fluid.mesh()
+    //     )
+    // ),
+    // Replaced by the following because the Clang compiler does not use
+    // std::move to transfer the result of the ternary operator
     volScalarField
     (
-        referencePhase
-      ? volScalarField
+        IOobject
         (
-            IOobject
-            (
-                IOobject::groupName("alpha", phaseName),
-                fluid.mesh().time().timeName(),
-                fluid.mesh(),
-                IOobject::NO_READ,
-                IOobject::AUTO_WRITE
-            ),
+            IOobject::groupName("alpha", phaseName),
+            fluid.mesh().time().timeName(),
+            fluid.mesh(),
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        referencePhase
+      ? volScalarField::New
+        (
+            IOobject::groupName("alpha", phaseName),
             fluid.mesh(),
             dimensionedScalar(dimless, 0)
         )
-      : volScalarField
+      : tmp<volScalarField>
         (
-            IOobject
+            new volScalarField
             (
-                IOobject::groupName("alpha", phaseName),
-                fluid.mesh().time().timeName(),
-                fluid.mesh(),
-                IOobject::MUST_READ,
-                IOobject::AUTO_WRITE
-            ),
-            fluid.mesh()
+                IOobject
+                (
+                    IOobject::groupName("alpha", phaseName),
+                    fluid.mesh().time().timeName(),
+                    fluid.mesh(),
+                    IOobject::MUST_READ,
+                    IOobject::AUTO_WRITE,
+                    false
+                ),
+                fluid.mesh()
+            )
         )
     ),
 